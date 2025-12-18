9dbb009061fdd32bed886fe0ec774ff1e93f3d0e
jambroongDev <akhm20182ti@nurulfikri.ac.id>
2025-12-18 13:36:41 +0700

    p

--- routes/channels.php
+++ routes/channels.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return $user->id === $id;
 });
 
 Broadcast::channel('user.{id}', function ($user, $id) {
-    \Log::info('🔐 Channel authentication attempt', [
+    Log::info('🔐 Channel authentication attempt', [
         'channel' => "user.{$id}",
         'user_id' => $user->id,
         'expected_id' => $id,
